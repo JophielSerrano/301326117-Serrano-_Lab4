4f859242eadef47ea5ff01f1954ae5989aba8257
JophielSerrano <jserra27@my.centennialcollege.ca>
2025-04-06 11:22:32 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using StringExtensionsLibrary;
+
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] testStrings = { "Hello", "world", "Testing", "" };
+
+            foreach (var str in testStrings)
+            {
+                Console.WriteLine($"'{str}' starts with uppercase: {str.StartsWithUpperCase()}");
+            }
+        }
+    }
+}
